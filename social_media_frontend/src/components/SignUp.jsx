793eacda209681f4041b2b2b32128f7d59ed134c
Udhay-WO <udhay.webosmotic@gmail.com>
2025-02-25 17:59:24 +0530

    signup base done

--- social_media_frontend/src/components/SignUp.jsx
+++ social_media_frontend/src/components/SignUp.jsx
@@ -1,187 +1,177 @@
-import 'react'
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import FormLabel from "@mui/material/FormLabel";
-import FormControl from "@mui/material/FormControl";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
-import MuiCard from "@mui/material/Card";
-import CryptoJS from "crypto-js";
-import { styled } from "@mui/material/styles";
-import { NavLink } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { v4 as uuidV4 } from "uuid";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, Controller } from "react-hook-form";
+import {
+  TextField,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+  Box,
+} from "@mui/material";
 import * as Yup from "yup";
-
-const Card = styled(MuiCard)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignSelf: "center",
-  width: "100%",
-  padding: theme.spacing(4),
-  gap: theme.spacing(2),
-  margin: "auto",
-  boxShadow:
-    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
-  [theme.breakpoints.up("sm")]: {
-    width: "450px",
-  },
-  ...theme.applyStyles("dark", {
-    boxShadow:
-      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
-  }),
-}));
-
-const SignUpContainer = styled(Stack)(({ theme }) => ({
-  height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
-  minHeight: "100%",
-  padding: theme.spacing(2),
-  [theme.breakpoints.up("sm")]: {
-    padding: theme.spacing(4),
-  },
-  "&::before": {
-    content: '""',
-    display: "block",
-    position: "absolute",
-    zIndex: -1,
-    inset: 0,
-    backgroundImage:
-      "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
-    backgroundRepeat: "no-repeat",
-    ...theme.applyStyles("dark", {
-      backgroundImage:
-        "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
-    }),
-  },
-}));
-
+import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
+import SnackBar from "./SnackBar";
+import { useState } from "react";
 const validationSchema = Yup.object({
-  name: Yup.string().required("Name is required."),
+  firstname: Yup.string()
+    .min(2, "First name must have atleast 2 characters")
+    .required("First name is required"),
+  lastname: Yup.string()
+    .min(2, "Last name must have atleast 2 characters")
+    .required("Last name is required"),
+  username: Yup.string().required("Username is required"),
   email: Yup.string()
-    .email("Please enter a valid email address.")
-    .required("Email address is required.")
-    ,
+    .email("Invalid email address")
+    .required("Email is required"),
   password: Yup.string()
-    .required("Password is required.")
-    .min(6, "Password must be at least 6 characters long."),
-  confirmPassword: Yup.string()
-    .required("Confirm password is required.")
-    .oneOf([Yup.ref("password"), null], "Passwords must match."),
-});
-function SignUp() {
-  const navigate = useNavigate();
+    .min(8, "Password must be at least 6 characters")
+    .required("Password is required"),
+  isPrivate: Yup.bool(),
 
+});
+
+const SignUp = () => {
+    const [open, setOpen] = useState(false);
+    const [message,setMessage] = useState("");
   const {
-    register,
+    control,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm({
     resolver: yupResolver(validationSchema),
+    defaultValues: {
+      firstname: "",
+      lastname: "",
+      username: "",
+      email: "",
+      password: "",
+      isPrivate: false,
+    },
   });
 
   const onSubmit = (data) => {
-    const { name, email, password } = data;
-    const secretKey = "my-secret-key";
-    const cipherText = CryptoJS.AES.encrypt(password, secretKey).toString();
-
-    const newUser = {
-      id: uuidV4(),
-      name,
-      email,
-      password: cipherText,
-      contact: [],
-    };
-    console.log(newUser)
-    navigate("/");
+    axios
+      .post("http://localhost:5000/sign-up", data)
+      .then((response) => {
+        console.log("Success:", response.data);
+        setOpen(true)
+        setMessage(response.data.message);
+      })
+      .catch((error) => console.error("Error:", error));
+    reset();
   };
 
   return (
-    <>
-      <CssBaseline enableColorScheme />
-      <SignUpContainer direction="column" justifyContent="space-between" height="100vh">
-        <Card variant="outlined">
-          <Typography component="h1" variant="h4" sx={{ width: "100%", fontSize: "1.5rem" }}>
-            Sign up
-          </Typography>
-          <Box
-            component="form"
-            sx={{ display: "flex", flexDirection: "column", gap: 1 }}
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <FormControl>
-              <FormLabel htmlFor="name">Full name</FormLabel>
-              <TextField
-                {...register("name")}
-                autoComplete="name"
-                fullWidth
-                placeholder="Enter name"
-                error={!!errors.name}
-                helperText={errors.name?.message}
-                color={errors.name ? "error" : "primary"}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <TextField
-                {...register("email")}
-                required
-                fullWidth
-                placeholder="your@email.com"
-                name="email"
-                autoComplete="email"
-                variant="outlined"
-                error={!!errors.email}
-                helperText={errors.email?.message}
-                color={errors.email ? "error" : "primary"}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="password">Password</FormLabel>
-              <TextField
-                {...register("password")}
-                required
-                fullWidth
-                name="password"
-                placeholder="••••••"
-                type="password"
-                variant="outlined"
-                error={!!errors.password}
-                helperText={errors.password?.message}
-                color={errors.password ? "error" : "primary"}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
-              <TextField
-                {...register("confirmPassword")}
-                required
-                fullWidth
-                name="confirmPassword"
-                placeholder="••••••"
-                type="password"
-                variant="outlined"
-                error={!!errors.confirmPassword}
-                helperText={errors.confirmPassword?.message}
-                color={errors.confirmPassword ? "error" : "primary"}
-              />
-            </FormControl>
-            <Button type="submit" fullWidth variant="contained">
-              Sign up
-            </Button>
-          </Box>
-          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-            <Typography sx={{ textAlign: "center" }}>
-              Already have an account? <NavLink to="/">Sign in</NavLink>
-            </Typography>
-          </Box>
-        </Card>
-      </SignUpContainer>
-    </>
+    <Box sx={{ maxWidth: 400, margin: "auto", padding: 2 }}>
+      <Typography variant="h5" gutterBottom>
+        User Registration
+      </Typography>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="firstname"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="First Name"
+              fullWidth
+              margin="normal"
+              error={!!errors.firstname}
+              helperText={errors.firstname?.message}
+            />
+          )}
+        />
+
+        <Controller
+          name="lastname"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Last Name"
+              fullWidth
+              margin="normal"
+              error={!!errors.lastname}
+              helperText={errors.lastname?.message}
+            />
+          )}
+        />
+
+        <Controller
+          name="username"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Username"
+              fullWidth
+              margin="normal"
+              error={!!errors.username}
+              helperText={errors.username?.message}
+            />
+          )}
+        />
+
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Email"
+              type="email"
+              fullWidth
+              margin="normal"
+              error={!!errors.email}
+              helperText={errors.email?.message}
+            />
+          )}
+        />
+
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Password"
+              type="password"
+              fullWidth
+              margin="normal"
+              error={!!errors.password}
+              helperText={errors.password?.message}
+            />
+          )}
+        />
+
+        <Controller
+          name="isPrivate"
+          control={control}
+          render={({ field }) => (
+            <FormControlLabel
+              control={
+                <Checkbox {...field} checked={field.value} color="primary" />
+              }
+              label="Private Profile"
+            />
+          )}
+        />
+
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          type="submit"
+          sx={{ mt: 2 }}
+        >
+          Submit
+        </Button>
+      </form>
+      <SnackBar open={open} set={setOpen} message={message}/>
+    </Box>
   );
-}
+};
 
 export default SignUp;
